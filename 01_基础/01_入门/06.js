090054f89e7e2fa0a38fc2753e0092f136a5d773
javakcfu123 <8031401@qq.com>
2019-07-24 12:14:29 +0800

    1

---
+++ 01_基础/01_入门/06.js
@@ -0,0 +1,4 @@
+//声明函数
+function fun(){
+	document.getElementById("info").innerHTML="<img src='../../img/ajax.jpg' />";
+}
---
+++ 04_文档_document/test.js
@@ -0,0 +1,6 @@
+//终极风格
+window.onload=function (){
+	document.getElementById("btn").onclick=function(){
+		alert(1);
+	}	
+};
